29eda5ef4695d8217d05019bc0f223bf4ed43193
cje98 <chlwldms120@naver.com>
2024-02-20 23:22:11 +0900

    Top 3 리스트 아이템 컴포넌트 생성

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+//import BoardItem from 'components/BoardItem';
+//import { latestBoardListMock } from 'mocks';
+import Top3Item from 'components/Top3Item';
+import { top3BoardListItem } from 'mocks';
 import BoardItem from 'components/BoardItem';
-import { latestBoardListMock } from 'mocks';
 
 function App() {
   return (
     <>
-      {latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem}/>)}
+      {/* {latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem}/>)} */}
+      <div style={{display: 'flex', justifyContent: 'center', gap:'24px'}}>
+        {top3BoardListItem.map(top3ListItem => <Top3Item top3ListItem={top3ListItem} />)}
+      </div>
     </>
   );
 }
--- board-front/src/mocks/index.ts
+++ board-front/src/mocks/index.ts
@@ -1,5 +1,7 @@
+import top3BoardListItem from './top-3-board-list.mock';
 import latestBoardListMock from './latest-board-list.mock';
 
 export{
+    top3BoardListItem,
     latestBoardListMock
 }

272dce597df7edaa4d5862e0f605c40d17a032cd
cje98 <chlwldms120@naver.com>
2024-02-22 15:55:57 +0900

    Input Box 컴포넌트 생성

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 //import BoardItem from 'components/BoardItem';
@@ -8,8 +8,12 @@ import { commentListMock, favoriteListMock, top3BoardListItem } from 'mocks';
 import BoardItem from 'components/BoardItem';
 import CommentItem from 'components/CommentItem';
 import FavoriteItem from 'components/FavoriteItem';
+import InputBox from 'components/InputBox';
 
 function App() {
+
+  const [value, setValue] = useState<string>('');
+
   return (
     <>
       {/* {latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem}/>)} */}
@@ -19,9 +23,10 @@ function App() {
       {/* <div style={{padding: '0 20px', display: 'flex', flexDirection: 'column', gap: '30px'}}>
         {commentListMock.map(commentListItem => <CommentItem commentListItem={commentListItem}/>)}
       </div> */}
-      <div style={{padding: '0 20px', display: 'flex', columnGap: '30px'}}>
+      {/* <div style={{padding: '0 20px', display: 'flex', columnGap: '30px'}}>
         {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem} />)}
-      </div>
+      </div> */}
+      <InputBox label={'이메일'} type={'text'} placeholder={'이메일 주소를 입력해주세요.'} value={value} setValue={setValue } error={true} />
     </>
   );
 }
---
+++ board-front/src/components/InputBox/index.tsx
@@ -0,0 +1,67 @@
+import {KeyboardEvent, ChangeEvent, Dispatch, SetStateAction, forwardRef } from 'react';
+import './style.css';
+
+//          interface : Input Box 컴포넌트 Properties          //
+interface Props{
+    label: string;
+    // ts 리터럴 타입 사용
+    type: 'text' | 'password'; 
+    placeholder: string;
+    value: string;
+    setValue: Dispatch<SetStateAction<string>>;
+    error: boolean;
+
+    // ? : 옵셔널, 필수 X
+    icon?: string;
+    onButtonClick?: () => void;
+    
+    message?: string;
+
+    // import 직접 넣어주면 됨
+    onKeyDown?: (event: KeyboardEvent<HTMLInputElement>)=> void;
+}
+
+//          component : Input Box 컴포넌트          //
+const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
+
+    //          state : properties          //
+    const {label, type, error, placeholder, value, icon, message} = props;
+    const {setValue, onButtonClick, onKeyDown} = props;
+
+    //          event handler : input 키 이벤트 처리 함수          //
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        // const {value} = event.target;
+        setValue(value);
+    }
+
+    const onKeydownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if(!onKeyDown) return;
+        onKeyDown(event);
+    }
+
+    //          render : Input Box 렌더링          //
+    return (
+        <div className='inputbox'>
+            <div className='inputbox-label'>{label}</div>
+            <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
+                {/* ref : 키보드 엔터치면 넘어가게 하는 역할 */}
+                <input ref={ref} type={type} className='input' placeholder={placeholder} value={value} onChange={onChangeHandler} onKeyDown={onKeydownHandler}/>
+                {/* onButtonClick이 undefined이 아니면 컴포넌트 랜더링 됨 */}
+                {onButtonClick !== undefined && (
+                    <div className='icon-button'>
+                                                            {/* 템플릿 리터럴 사용 */}
+                        {icon !== undefined && (<div className={`icon ${icon}`}></div>)}
+                    </div>
+                )}
+            </div>
+            {message !== undefined &&(
+                <div className='inputbox-message'>{'비밀번호는 8자 이상 입력해주세요.'}</div>
+            )}
+        </div>
+
+    )
+
+});
+
+export default InputBox;

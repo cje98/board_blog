48aeea1e58be9c0d9f5bcb9bab4e16fc92a4700f
cje98 <chlwldms120@naver.com>
2024-03-13 17:13:08 +0900

    게시물 상세보기 상단 스타일 작성

--- board-front/src/views/Board/Detail/index.tsx
+++ board-front/src/views/Board/Detail/index.tsx
@@ -6,11 +6,22 @@ import { commentListMock, favoriteListMock } from 'mocks';
 import CommentItem from 'components/CommentItem';
 import Pagination from 'components/Pagination';
 
+import defaultProfileImage from 'assets/image/default-profile-image.png';
+import { url } from 'inspector';
+
 //          component : 게시물 상세 화면 컴포넌트          //
 export default function BoardDetail() {
 
   //          component : 게시물 상세 상단 컴포넌트          // 
   const BoardDetailTop = () => {
+
+    //          state : more 버튼 상태          //
+    const [showMore, setShowMore] = useState<boolean>(false);
+
+    //          event handler : more 버튼 클릭 이벤트 처리          //
+    const onMoreButtonClickHandler = () => {
+      setShowMore(!showMore);
+    }
 
     //          render : 게시물 상세 상단 컴포넌트 렌더링          //
     return (
@@ -19,25 +30,28 @@ export default function BoardDetail() {
           <div className='board-detail-title'>{'오늘 점심은 초콜릿이야.'}</div>
           <div className='board-detail-top-sub-box'>
             <div className='board-detail-write-info-box'>
-              <div className='board-detail-writer-profile-image'></div>
+              <div className='board-detail-writer-profile-image' style={{backgroundImage:`url(${defaultProfileImage})`}}></div>
               <div className='board-detail-writer-nickname'>{'나는 개똥벌레'}</div>
               <div className='board-detail-info-divider'>{'\|'}</div>
               <div className='board-detail-write-date'>{'2024. 03. 13'}</div>
             </div>
-            <div className='icon-button'>
+            <div className='icon-button' onClick={onMoreButtonClickHandler}>
               <div className='icon more-icon'></div>
             </div>
-            <div className='board-detail-more-box'>
-              <div className='board-detail-update-button'>{'수정'}</div>
-              <div className='divider'></div>
-              <div className='board-detail-delete-button'>{'삭제'}</div>
-            </div>
+            {/* showMore가 true이면 수정/삭제 버튼 보이게 상태 처리 */}
+            {showMore &&
+              <div className='board-detail-more-box'>
+                <div className='board-detail-update-button'>{'수정'}</div>
+                <div className='divider'></div>
+                <div className='board-detail-delete-button'>{'삭제'}</div>
+              </div>
+            }
           </div>
         </div>
         <div className='divider'></div>
         <div className='board-detail-top-main'>
           <div className='board-detail-main-text'>{'오늘 저녁은 닭볶음탕이야.'}</div>
-          <div className='board-detail-main-image'></div>
+          <img className='board-detail-main-image' src='https://www.nongmin.com/-/raw/srv-nongmin/data2/content/image/2024/03/11/.cache/512/20240311500526.jpg'></img>
         </div>
       </div>
     );

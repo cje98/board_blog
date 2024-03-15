42146b4038a05653f98c232b48f44158b36a262f
cje98 <chlwldms120@naver.com>
2024-03-15 12:24:38 +0900

    게시물 상세 페이지 이벤트 처리

---
+++ board-front/src/mocks/board.mock.ts
@@ -0,0 +1,17 @@
+import { Board } from "types/interface";
+
+const boardMock: Board = {
+    boardNumber: 1,
+    title: '오늘 저녁은 없다.오늘 저녁은 없다.오늘 저녁은 없다.오늘 저녁은 없다.오늘 저녁은 없다.오늘 저녁은 없다.오늘 저녁은 없다.',
+    content: '내일 저녁도 없다.내일 저녁도 없다.내일 저녁도 없다.내일 저녁도 없다.내일 저녁도 없다.내일 저녁도 없다.내일 저녁도 없다.내일 저녁도 없다.내일 저녁도 없다.',
+    boardImageList: [
+        'https://tvdaily.co.kr/upimages/gisaimg/201603/1457149673_1084155.jpg',
+        'https://image.ajunews.com/content/image/2023/10/20/20231020180306328500.jpg'
+    ],
+    writeDatetime: '2024. 03. 14.',
+    writerEmail: 'email3@gmail.com',
+    writerNickname: '나는 개똥벌레',
+    writerProfileImage: 'https://image.ajunews.com/content/image/2023/10/20/20231020180306328500.jpg'
+}
+
+export default boardMock;
--- board-front/src/mocks/index.ts
+++ board-front/src/mocks/index.ts
@@ -1,9 +1,11 @@
+import boardMock from './board.mock';
 import favoriteListMock from './favorite-list.mock';
 import commentListMock from './comment-list.mock';
 import top3BoardListItem from './top-3-board-list.mock';
 import latestBoardListMock from './latest-board-list.mock';
 
 export{
+    boardMock,
     favoriteListMock,
     commentListMock,
     top3BoardListItem,
